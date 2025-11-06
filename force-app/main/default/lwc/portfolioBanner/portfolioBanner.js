44509092dfcfd38f175af3e1e4d99484876774c4
jyotiraditya0607 <jyotiradityabiswal64@gmail.com>
2025-11-06 07:09:09 +0000

    feat: change the social urls dynamically

--- force-app/main/default/lwc/portfolioBanner/portfolioBanner.js
+++ force-app/main/default/lwc/portfolioBanner/portfolioBanner.js
@@ -1,4 +1,4 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 import PortfolioAssets from '@salesforce/resourceUrl/PortfolioAssets'
 import {getRecord, getFieldValue} from 'lightning/uiRecordApi';
 import FULLNAME from '@salesforce/schema/Portfolio__c.Full_Name__c'
@@ -7,23 +7,24 @@ import COMPANY_NAME from '@salesforce/schema/Portfolio__c.Company_Name__c'
 import DESIGNATION from '@salesforce/schema/Portfolio__c.Designation__c'
 
 export default class PortfolioBanner extends LightningElement {
-
-    linkedinUrl = 'https://www.linkedin.com/in/jyotiraditya-biswal/'
-    twitterUrl = 'https://x.com/jyotiraditya_2'
-    githubUrl = 'https://github.com/jyotiraditya0607'
-    trailheadUrl = 'https://www.salesforce.com/trailblazer/jyotiradityabiswal'
-    // blogUrl = 'https://www.salesforcetroop.com/'
+    @api recordId //= 'a01g5000002RML8AAO'
+    @api linkedinUrl// = 'https://www.linkedin.com/in/jyotiraditya-biswal/'
+    @api twitterUrl //= 'https://x.com/jyotiraditya_2'
+    @api githubUrl //= 'https://github.com/jyotiraditya0607'
+    @api trailheadUrl// = 'https://www.salesforce.com/trailblazer/jyotiradityabiswal'
+    @api blogUrl //= 'https://www.salesforcetroop.com/'
+    @api youtubeUrl // = 'I do not have any right now'
 
 
     userPic = `${PortfolioAssets}/PortfolioAssets/jyotiraditya.png`
     linkedin = `${PortfolioAssets}/PortfolioAssets/Social/linkedin.svg`
-    // youtube = `${PortfolioAssets}/PortfolioAssets/Social/youtube.svg`
+    youtube = `${PortfolioAssets}/PortfolioAssets/Social/youtube.svg`
     github = `${PortfolioAssets}/PortfolioAssets/Social/github.svg`
     twitter = `${PortfolioAssets}/PortfolioAssets/Social/x.svg`
     trailhead = `${PortfolioAssets}/PortfolioAssets/Social/trailhead1.svg`
-    // blog = `${PortfolioAssets}/PortfolioAssets/Social/blogger.svg`
+    blog = `${PortfolioAssets}/PortfolioAssets/Social/blogger.svg`
 
-    recordId = 'a01g5000002RML8AAO'
+    
     @wire(getRecord, {recordId: '$recordId', fields:[FULLNAME,COMPANT_LOCATION,COMPANY_NAME,DESIGNATION]}) 
     portfolioData
     // portfolioHandler({data, error}) {
