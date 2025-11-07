b2137b795f94b75937c13e896fc482eef50ab8fe
jyotiraditya0607 <jyotiradityabiswal64@gmail.com>
2025-11-07 11:20:43 +0000

    feat: added child components of portfolioUserDetailsWrapperClass

---
+++ force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
@@ -0,0 +1,12 @@
+import { LightningElement, api } from 'lwc';
+
+export default class PortfolioUserDetails extends LightningElement {
+    @api recordId
+    @api objectApiName
+    @api resumeUrl
+
+    downloadResume(){
+        window.open(this.resumeUrl,"_blank")
+    }
+}
+
